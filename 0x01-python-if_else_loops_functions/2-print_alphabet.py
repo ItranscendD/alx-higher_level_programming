9f3f909fee6e96a0b6a06a6b176184ed66bdde4e
ItranscendD <danielcasey988@gmail.com>
2023-11-29 09:28:35 -0800

    test 4

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 for i in range(ord('a'), ord('z') + 1):
-    print("{}".format(chr(i)), end='')
+    print("".join("{}".format(chr(i)) for i in range(ord('a'), ord('z') + 1)))
 
 # Print a newline at the end
 print()

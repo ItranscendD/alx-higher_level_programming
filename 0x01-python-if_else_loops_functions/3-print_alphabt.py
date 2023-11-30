4f94ad2b2e874567f595a383534416fa22a737e7
ItranscendD <danielcasey988@gmail.com>
2023-11-30 07:36:29 -0800

    corrections

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
 # Use a single print function with string format to print the alphabet, excluding 'q' and 'e'
-print("".join("{}".format(chr(i)) for i in range(ord('a'), ord('z') + 1) if chr(i) not in ('q', 'e')), end='')
+print(
+    "".join(
+        "{}".format(chr(i))
+        for i in range(ord('a'), ord('z') + 1)
+        if chr(i) not in ('q', 'e')
+    ),
+    end=''
+)

147858750fb83a6c94cf9458ba7d226b3eb72ea0
ItranscendD <danielcasey988@gmail.com>
2023-11-30 23:14:14 -0800

    corrections

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,6 +3,6 @@ import sys
 
 if __name__ == "__main__":
     argc = len(sys.argv) - 1
-    print("{:d} argument{}{}".format(argc, "" if argc == 1 else "s", ":" if argc > 0 else "."))
+    print("{} argument{}{}".format(argc, '' if argc == 1 else 's', ':' if argc > 0 else '.'))
     for i in range(argc):
         print("{:d}: {}".format(i + 1, sys.argv[i + 1]))

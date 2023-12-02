ec19882d694907b3aaed35a01bb0a67e78cdce46
ItranscendD <danielcasey988@gmail.com>
2023-12-01 23:19:36 -0800

    updated

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -7,6 +7,8 @@ def no_c(my_string):
     return filtered_string
 
 # Example usage:
+
+
 if __name__ == "__main__":
     test_strings = ["Best School", "Chicago", "C is fun!"]
     for test_string in test_strings:

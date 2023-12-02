b2859a0e01585c6e92c46010e02e5990d7f87b53
ItranscendD <danielcasey988@gmail.com>
2023-12-01 23:16:20 -0800

    updated

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -8,11 +8,13 @@ def new_in_list(my_list, idx, element):
     return new_list
 
 # Example usage:
+
+
 if __name__ == "__main__":
     original_list = [1, 2, 3, 4, 5]
     new_index = 2
     new_element = 99
-    
+
     # Test case
     modified_list = new_in_list(original_list, new_index, new_element)
     print("Original List:", original_list)

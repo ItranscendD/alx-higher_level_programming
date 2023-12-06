0ee1f8feee0c628d48bbd89fea6b7a474a55773c
ItranscendD <danielcasey988@gmail.com>
2023-12-06 14:34:12 -0800

    simple delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,19 +1,7 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+def simple_delete(a_dictionary, key=""):
+    """Deletes a key in a dictionary."""
+    if key in a_dictionary:
+        del a_dictionary[key]
     return a_dictionary
-
-# usage:
-
-
-if __name__ == "__main__":
-    my_dict = {'language': "C", 'number': 89, 'track': "Low level"}
-
-    # Update existing key
-    update_dictionary(my_dict, 'language', 'Python')
-    print(my_dict)
-
-    # Add a new key
-    update_dictionary(my_dict, 'city', 'San Francisco')
-    print(my_dict)

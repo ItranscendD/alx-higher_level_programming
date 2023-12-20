73d20f08d7e03e562bbeacd55a2576b8631bbe22
ItranscendD <danielcasey988@gmail.com>
2023-12-20 05:01:26 -0800

    exception trial

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,7 +9,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += (a ** b) / i
-        except:
+        else:
             result += b + a
             break
 
